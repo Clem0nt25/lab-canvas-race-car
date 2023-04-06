dccecf36355ed4a8aee23ee58f13f192b9b8839c
Clemens Marckwardt <clemens.marckwardt@gmail.com>
2023-04-06 15:31:59 +0200

    total fail

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
+
 window.onload = () => {
+
+
+  const gameIntro = document.querySelector("game-intro")
+  const canvas = document.getElementById("canvas")
+
+  const boardBg = new Image()
+  boardBg.src = '/images/road.png'
+
+  const ctx = canvas.getContext('2d')
+
+  
+  const animate = () => {
+    ctx.drawImage(boardBg, 0, 0, 500, 700)
+
+  }
+
+  animate()
+
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    console.log("Game started")
+    animate()
+    gameIntro.styles.display = "none"
+    
+  }
+
+
+
+
+
 };
